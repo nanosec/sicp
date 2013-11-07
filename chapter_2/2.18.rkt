29abf751cceb4d879d9bc0665a1e07ef06ffe0f0
nshen <nshen@mint-tea.none>
2013-11-08 01:57:50 +0900

    2.18.rkt: Rename my 'append' to more accurate 'insert-at-end'

--- chapter_2/2.18.rkt
+++ chapter_2/2.18.rkt
@@ -3,14 +3,14 @@
 ; reverse : list -> list
 ; to reverse a-list
 (define (reverse a-list)
-  ; append : X list -> list
-  (define (append x a-list)
+  ; insert-at-end : X list-> list
+  (define (insert-at-end x a-list)
     (if (null? a-list)
         (list x)
-        (cons (car a-list) (append x (cdr a-list)))))
+        (cons (car a-list) (insert-at-end x (cdr a-list)))))
   (if (null? a-list)
       a-list
-      (append (car a-list) (reverse (cdr a-list)))))
+      (insert-at-end (car a-list) (reverse (cdr a-list)))))
 ; Examples/Tests: 
 (equal? (reverse null) null)
 (equal? (reverse (list 1)) (list 1))
